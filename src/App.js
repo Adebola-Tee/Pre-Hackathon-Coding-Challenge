22975710fcc032eccd9c7d1b8dba471ada6c5946
Adebola-Tee <taiwooloyede49@gmail.com>
2023-09-20 13:39:59 +0100

    tailwindcss functioning

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p className='text-red-800 font-extrabold'>Hello tailwindcss</p>
+    </div>
+  );
+}
+
+export default App;
